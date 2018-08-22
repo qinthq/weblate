9d60eccc6e850e6e25bee06e7a5b570eac753143
Philamer Sune <pvsune@hotmail.com>
2018-08-22 14:39:25 +0800

    :sparkles: Add custom Machine Translation service using Elasticsearch.

---
+++ weblate/machinery/elasticsearch.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2018 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from __future__ import unicode_literals
+
+import requests
+
+from django.conf import settings
+
+from weblate.lang.models import Language
+from weblate.logger import LOGGER
+from weblate.memory.storage import TranslationMemory
+from weblate.machinery.base import MachineTranslation
+
+
+class ESTranslation(MachineTranslation):
+    """Translation service using strings already translated in Weblate."""
+    name = 'Elasticsearch Translation Memory'
+    rank_boost = 1
+    cache_translations = False
+
+    def convert_language(self, language):
+        return Language.objects.get(code=language)
+
+    def is_supported(self, source, language):
+        """Any language is supported."""
+        return True
+
+    def format_unit_match(self, text, target, similarity, origin):
+        """Format match to translation service result."""
+        return (
+            target,
+            similarity,
+            '{0} ({1})'.format(
+                self.name,
+                origin,
+            ),
+            text,
+        )
+
+    def download_translations(self, source, language, text, unit, user):
+        """Download list of possible translations from a service."""
+        es_search_url = '{}/{}/_search'.format(
+            settings.MT_ES_URL, 'weblate_tm'
+        )
+        try:
+            r = requests.get(
+                es_search_url,
+                json={'query': {'match': {'source': text}}},
+                timeout=20,
+            )
+            r.raise_for_status()
+            res_ = r.json()
+        except requests.exceptions.HTTPError:
+            LOGGER.error(
+                'Querying Elasticsearch: "%s" failed.', es_search_url
+            )
+            raise
+
+        return [
+            self.format_unit_match(
+                u['_source']['source'],
+                u['_source']['target'],
+                u['_score'],
+                u['_type'],
+            ) for u in res_['hits']['hits']
+        ]
